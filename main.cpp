a7c6d495beb863db92a611f05d555ff01e2c2d39
Abdalla Fadl <abdalla.fadle2001@gmail.com>
2023-02-24 08:15:01 +0200

    first commit

--- main.cpp
+++ main.cpp
@@ -11,21 +11,11 @@ int  getSum(vector <int> numbers){
 }
 
 
-double getAverage(vector <int> numbers){
-    int sum = 0 ;
-    double avg = 0 ;
-    for(int i = 0 ;i<numbers.size();i++){
-        sum += numbers[i];
-    }
-    avg =(double) sum / numbers.size();
-    return avg ;
-}
 
 
 int main()
 {
     vector <int> numbers = {1,2,3,4,5,6,7,8,9,10};
     cout<<getSum(numbers)<<endl;
-    cout<<getAverage(numbers)<<endl;
     return 0;
 }
